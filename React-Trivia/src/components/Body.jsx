60215d23f0331126792be286072da312b9e1938c
schlajo <schlajo1981@gmail.com>
2024-10-21 20:51:55 -0500

    Created header and a button beneath it.

---
+++ React-Trivia/src/components/Body.jsx
@@ -0,0 +1,9 @@
+const Body = () => {
+    return (
+        <body className="card">
+            <button className ="btn">Start</button>
+        </body>
+    )
+}
+
+export default Body;
---
+++ React-Trivia/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import './Header.css';
+
+
+
+const Header = () => {
+    return (
+        <header className="card">
+            <h1>Country Capital Trivia</h1>
+        </header>
+    )
+}
+
+export default Header;
